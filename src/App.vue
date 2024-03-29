32bae8299940543e647a5446d51474a8b412085e
abdirisak009 <abdirisakmoha009@gmail.com>
2024-03-29 09:34:45 +0300

    commit one

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-import Navbar from './components/Navbar.vue';
+
 import Footer from './components/Footer.vue';
 
 // Refactoring!
@@ -8,12 +8,49 @@ import Footer from './components/Footer.vue';
 </script>
 
 <template class="bg-gray-200">
-  <Navbar />
 
-  <div class="py-10"></div>
+<nav class="mx-auto max-w-6xl border border-grey-600 bg-gray-100 shadow-lg">
 
+
+<div class="flex justify-between items-center">
+  <div class="flex space-x-8">
+    <div>
+      <RouterLin to="/">
+      <a href="#">
+        <span class="block  p-4 font-bold text-lg text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-blue-500">Doctor App</span>
+      </a>
+      </RouterLin>
+    </div>
+
+    <ul class="flex space-x-4 items-center">
+      <ll>
+        <RouterLink to="#" class="nav-item">Home</RouterLink>
+      </ll>
+
+       <ll>
+        <RouterLink to="#" class="nav-item">Doctors</RouterLink>
+      </ll>
+
+       <ll>
+        <RouterLink to="#" class="nav-item">Contact</RouterLink>
+      </ll>
+    </ul>
+
+
+   
+  </div>
+
+   <div class="flex space-x-2 px-4">
+      <RouterLink to="/login" class="button">Login</RouterLink>
+       <RouterLink to="/signup" class="button-pink">Signup</RouterLink>
+    </div>
+</div>
+
+
+</nav>
+  
   <RouterView />
-  <Footer />
+
 
   <!-- Footer will go here -->
 </template>
--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,15 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
-import Login from './pages/Login.vue';
-import Doctors from './pages/Doctors.vue';
-import Home from './pages/Home.vue';
-import Contact from './pages/Contact.vue'
-import Signup from './pages/Signup.vue';
-import one from './pages/booking1.vue'
-import two from './pages/booking2.vue'
+
 
 import './style.css';
 
 const router = createRouter({
   routes: [
-    { path: '/', component: Home },
-    { path: '/login', component: Login },
-    { path: '/doctors', component: Doctors },
-    { path: '/contact', component: Contact },
-    { path: '/signup', component: Signup },
-    { path: '/one', component: one },
-    { path: '/two', component: two },
+    
+    
+   
   ],
   history: createWebHistory(),
 });
